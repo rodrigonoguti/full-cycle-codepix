d4f5d4a372ce86922bb5d2773fc4dc962f4a3887
Rodrigo Haruo Noguti <rodrigonoguti@gmail.com>
2022-04-04 16:06:39 -0300

    implement domain models base

---
+++ domain/model/base.go
@@ -0,0 +1,17 @@
+package model
+
+import (
+	"time"
+
+	"github.com/asaskevich/govalidator"
+)
+
+func init() {
+	govalidator.SetFieldsRequiredByDefault(true)
+}
+
+type Base struct {
+	ID        string    `json:"id" valid:"uuid"`
+	CreatedAt time.Time `json:"created_at" valid:"-"`
+	UpdateAt  time.Time `json:"update_at" valid:"-"`
+}
